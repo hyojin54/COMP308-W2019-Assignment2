645608a90af3840971ce3bf2fe705c6c710aadb0
Hyojin Kim <akumada54@gmail.com>
2019-03-30 15:50:05 -0400

    Angular Read Contact List

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { ProjectsComponent } from './pages/projects/projects.component';
 import { ServicesComponent } from './pages/services/services.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent, data: {title: 'Home'}},
@@ -16,6 +17,7 @@ const routes: Routes = [
   {path: 'projects', component: ProjectsComponent, data: {title: 'Projects'}},
   {path: 'services', component: ServicesComponent, data: {title: 'Services'}},
   {path: 'contact', component: ContactComponent, data: {title: 'Contact'}},
+  {path: 'contact/contact-list', component: ContactListComponent, data: {title: 'Contact List'}},
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: '**', component: PageNotFoundComponent}
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,7 +1,11 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+// Components
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './partials/header/header.component';
 import { FooterComponent } from './partials/footer/footer.component';
@@ -13,6 +17,10 @@ import { ContactComponent } from './pages/contact/contact.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
+// Services
+import { FlashMessagesModule, FlashMessagesService } from 'angular2-flash-messages';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -20,17 +28,21 @@ import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.com
     FooterComponent,
     BasePageComponent,
     HomeComponent,
-    ServicesComponent,
     AboutComponent,
-    ContactComponent,
     ProjectsComponent,
-    PageNotFoundComponent
+    ServicesComponent,
+    ContactComponent,
+    PageNotFoundComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    FlashMessagesModule
   ],
-  providers: [],
+  providers: [FlashMessagesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
